f32beb3adcdf06f3a6c389adf3b2fa3457e60cd1
Kevin Pessetto <kjpessetto@gmail.com>
2019-06-03 18:47:27 -0500

    Server and file tree setup

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Product(){
+    return (
+        <Route>
+            <div>
+                Product
+            </div>
+        </Route>
+    )
+}
